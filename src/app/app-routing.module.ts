447d03e01aa0fbcafad101e7e4c7147e2a4efaa5
becelot <benedict.becker93@gmail.com>
2019-09-13 14:33:18 +0200

    Added basic filesystem routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FileViewerComponent} from './components/file-viewer/file-viewer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'files', children: [ { path: '**', component: FileViewerComponent } ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { FileViewerComponent } from './components/file-viewer/file-viewer.component';
+import { DocumentComponent } from './components/document/document.component';
+import {AngularSvgIconModule} from 'angular-svg-icon';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileViewerComponent,
+    DocumentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AngularSvgIconModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
