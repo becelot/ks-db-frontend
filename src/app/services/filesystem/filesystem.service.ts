7456cc1787eac34116457e34f53a36cb4d8b438e
becelot <benedict.becker93@gmail.com>
2019-09-13 14:42:26 +0200

    Filesystem now provides a current path

--- src/app/services/filesystem/filesystem.service.ts
+++ src/app/services/filesystem/filesystem.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable, Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilesystemService {
 
-  private currentPath = [];
+  private _currentPath: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
   constructor() { }
+
+  get currentPath(): Observable<string[]> {
+    return this._currentPath.asObservable();
+  }
+
+  public navigateNext(document: string) {
+    const current = this._currentPath.getValue();
+    current.push(document)
+    this._currentPath.next(current);
+  }
 }
