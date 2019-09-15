9b123675ecb5d5e22248a267575ef2c5ca811e7d
becelot <benedict.becker93@gmail.com>
2019-09-15 17:30:16 +0200

    Protected files route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FileViewerComponent} from './components/file-viewer/file-viewer.component';
+import {StartpageComponent} from './components/startpage/startpage.component';
+import {AuthorizationComponent} from './components/auth/authorization/authorization.component';
+import {LoginComponent} from './components/auth/login/login.component';
+import {LayoutComponent} from './components/layout/layout.component';
 
 
 const routes: Routes = [
-  { path: 'files', children: [ { path: '**', component: FileViewerComponent } ] }
+  { path: '', component: LayoutComponent, children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'start'
+      },
+      {
+        path: 'start',
+        component: StartpageComponent
+      },
+      { path: 'files',
+        children: [ { path: '**', component: FileViewerComponent } ]
+      }
+    ]
+  },
+  { path: 'auth', component: AuthorizationComponent, children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
