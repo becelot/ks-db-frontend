4d57b543e574c052ceed41fc09cba9662a3afad4
becelot <benedict.becker93@gmail.com>
2019-09-15 17:32:45 +0200

    Added authorization page

---
+++ src/app/components/auth/authorization/authorization.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'ks-authorization',
+  templateUrl: './authorization.component.html',
+  styleUrls: ['./authorization.component.scss']
+})
+export class AuthorizationComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
