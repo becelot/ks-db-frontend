118535421b0d8d800954227e864014b5f8bce43e
becelot <benedict.becker93@gmail.com>
2019-09-15 17:34:23 +0200

    Added startpage application

---
+++ src/app/components/startpage/startpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ks-startpage',
+  templateUrl: './startpage.component.html',
+  styleUrls: ['./startpage.component.scss']
+})
+export class StartpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
