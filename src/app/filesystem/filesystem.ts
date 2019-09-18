3e8555813d1deabf12877f4e7dfbb162754dd88b
becelot <benedict.becker93@gmail.com>
2019-09-18 15:59:06 +0200

    The filesystem has now the ability to create a new folder by given path

--- src/app/filesystem/filesystem.ts
+++ src/app/filesystem/filesystem.ts
@@ -1,6 +1,30 @@
 import {Folder} from './folder';
-
+import {Document} from './document';
 
 export class Filesystem {
   public root: Folder = new Folder();
+
+  public resolveOrCreateDocument(path: string): Document {
+    const childs: string[] = path.split('/');
+
+    let last: Folder = this.root;
+    let current: Document = this.root;
+    for (const child in childs) {
+      if (current instanceof Folder) {
+        last = current;
+        const tmp = current.getFile(child);
+
+        if (!!tmp) {
+          current = tmp;
+        } else {
+          current = new Folder(child);
+          last.addDocument(current);
+        }
+      } else {
+        throw new Error('Path is invalid');
+      }
+    }
+
+    return current;
+  }
 }
