3f06fb1f523dd2a9c57fee96d45a0d551ee02659
becelot <benedict.becker93@gmail.com>
2019-09-18 15:57:35 +0200

    Implemented basic document view

--- src/app/components/file-viewer/file-viewer.component.ts
+++ src/app/components/file-viewer/file-viewer.component.ts
@@ -1,9 +1,23 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FilesystemService} from '../../services/filesystem/filesystem.service';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
 import {AmplifyService} from 'aws-amplify-angular';
 import {APIClass} from 'aws-amplify';
 import {Doctype} from '@angular/compiler/src/i18n/serializers/xml_helper';
+import {Document} from '../../filesystem/document';
+import {Folder} from '../../filesystem/folder';
+
+
+export enum DocType {
+  DOC_FOLDER,
+  DOC_FILE
+}
+
+
+export interface IDocs {
+  name: string;
+  type: DocType;
+}
 
 @Component({
   selector: 'ks-file-viewer',
@@ -12,20 +26,48 @@ import {Doctype} from '@angular/compiler/src/i18n/serializers/xml_helper';
 })
 export class FileViewerComponent implements OnInit {
 
+  public docs: Document[] = [];
+
   constructor(private filesystem: FilesystemService,
               private router: Router,
               private amplifyService: AmplifyService) {
+    /*this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updatePath();
+      }
+    });*/
+
+    this.updatePath();
+  }
+
+  private async updatePath() {
+    let path: string = this.router.url.replace('/files', '');
+
+    // Slice pre and post slashes of path
+    if (path[0] === '/') {
+      path = path.slice(1, path.length);
+    }
+
+    if (path[path.length - 1] === '/') {
+      path = path.slice(0, path.length - 1);
+    }
+
+    const doc: Document = await this.filesystem.resolveDocument(path);
+    console.log(doc);
+    if (doc instanceof Folder) {
+      this.docs = [...doc.content];
+    }
+
   }
 
   ngOnInit() {
   }
 
   public async createSomeDocument() {
-    console.log('test');
     const api: APIClass = this.amplifyService.api();
 
     try {
-      const result = await api.post('CloudEndpoint', 'files/create', {
+      const result = await api.post('LocalEndpoint', 'files/create', {
         headers: {
           // Authorization: user.signInUserSession.idToken.jwtToken
         },
@@ -36,8 +78,6 @@ export class FileViewerComponent implements OnInit {
           date: 0
         }
       });
-
-      console.log(result);
     } catch (e) {
 
     }
