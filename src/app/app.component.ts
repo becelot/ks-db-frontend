fd069fafc332efae3f57dbcbeef19674c7546cb8
becelot <benedict.becker93@gmail.com>
2019-09-21 12:28:16 +0200

    Very basic toolbar integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {AmplifyService} from 'aws-amplify-angular';
 import {Auth, AuthClass} from 'aws-amplify';
 import {AuthState} from 'aws-amplify-angular/dist/src/providers';
 import {CognitoUser, CognitoUserSession} from 'amazon-cognito-identity-js';
+import {MatIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,10 @@ import {CognitoUser, CognitoUserSession} from 'amazon-cognito-identity-js';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor (icons: MatIconRegistry, domSanitizer: DomSanitizer) {
+    icons.addSvgIcon('add_doc', domSanitizer.bypassSecurityTrustResourceUrl('assets/newdoc.svg'));
+    icons.addSvgIcon('add_folder', domSanitizer.bypassSecurityTrustResourceUrl('assets/newfolder.svg'));
+    icons.addSvgIcon('edit_doc', domSanitizer.bypassSecurityTrustResourceUrl('assets/edit.svg'));
 
+  }
 }
